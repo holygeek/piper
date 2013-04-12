feb804e6a4e1307b01170506dc8f352cc3bd2f35
Nazri Ramliy <ayiehere@gmail.com>
2013-04-12 11:07:30 +0800

    initial

---
+++ piper.go
@@ -0,0 +1,31 @@
+package piper
+
+import (
+	"bufio"
+	"log"
+	"os/exec"
+)
+
+// MustPipe returns stdout and stderr line scanners (bufio.Scanner) for the
+// the command created from the given argument. It calls log.Fatal() if the
+// command could not be started, or if the stdout or stderr plumbings failed
+func MustPipe(exe string, args ...string) (*bufio.Scanner, *bufio.Scanner) {
+	cmd := exec.Command(exe, args...)
+
+	stderr, err := cmd.StderrPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal("MustPipe:", exe, err)
+	}
+
+	return bufio.NewScanner(stdout), bufio.NewScanner(stderr)
+}
